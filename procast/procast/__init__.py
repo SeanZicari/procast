2212f08ec343642198c17cd57bf4938a9cc302b8
Sean Zicari <sean.zicari@gmail.com>
2016-03-05 15:03:43 -0600

    Getting form submission working.

--- procast/procast/__init__.py
+++ procast/procast/__init__.py
@@ -1,13 +1,18 @@
 from pyramid.config import Configurator
+from pyramid.session import SignedCookieSessionFactory
+
+my_session_factory = SignedCookieSessionFactory('tickles')
 
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings)
+    config.set_session_factory(my_session_factory)
     config.include('pyramid_jinja2')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.add_route('faq', '/faq')
+    config.add_route('contactme', '/contactme')
     config.scan()
     return config.make_wsgi_app()
--- procast/procast/views.py
+++ procast/procast/views.py
@@ -1,4 +1,9 @@
+import logging
+
 from pyramid.view import view_config
+import pyramid.httpexceptions as exc
+
+log = logging.getLogger(__name__)
 
 
 @view_config(route_name='home', renderer='templates/index.jinja2')
@@ -9,3 +14,9 @@ def home(request):
 @view_config(route_name='faq', renderer='templates/faq.jinja2')
 def faq(request):
     return {}
+
+
+@view_config(route_name='contactme', request_method='POST')
+def contactme(request):
+    request.session.flash("We'll reach out to you shortly to get started!", queue='contact_form')
+    return exc.HTTPFound(request.route_url('home'))
