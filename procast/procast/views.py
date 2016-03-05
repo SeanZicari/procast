dc149bceb9caa989a12b88e0ef723bf30ea6340b
Sean Zicari <sean.zicari@gmail.com>
2016-03-05 12:50:32 -0600

    Added FAQ page.

--- procast/procast/views.py
+++ procast/procast/views.py
@@ -2,5 +2,10 @@ from pyramid.view import view_config
 
 
 @view_config(route_name='home', renderer='templates/index.jinja2')
-def my_view(request):
-    return {'project': 'procast'}
+def home(request):
+    return {}
+
+
+@view_config(route_name='faq', renderer='templates/faq.jinja2')
+def faq(request):
+    return {}
